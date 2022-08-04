caeda449a588fc7795764fca6f7f6113a1816e0d
ywangnon <ywangnon123@gmail.com>
2022-08-04 18:50:10 +0900

    refactor: finding hobbies 수정
    반복되는 뷰 foreach로 정리
    텍스트필드 추가

--- Swiftly Code/FindingHobbies/FindingHobbies/ContentView.swift
+++ Swiftly Code/FindingHobbies/FindingHobbies/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var hobby: String = ""
+    @State var categories: [String] = ["Polo", "Swimming", "Dancing", "Singing"]
+    
     var body: some View {
         VStack {
+            // 상단 로고
             HStack {
                 Text("Hobbies")
-                    .font(.system(size: 50,
-                                  weight: .heavy))
                 Spacer()
                 Image("photo")
                     .resizable()
@@ -21,13 +23,17 @@ struct ContentView: View {
                     .clipShape(Circle())
             }
             
+            // 텍스트 입력창
             HStack {
                 HStack {
-                    Text("Search a Hobby")
+                    TextField("Search a Hobby", text: $hobby)
                         .padding()
-                    Spacer()
-                    Image(systemName: "magnifyingglass")
-                        .padding()
+                    Button {
+                        print("search")
+                    } label: {
+                        Image(systemName: "magnifyingglass")
+                            .padding()
+                    }
                 }
                 .padding(EdgeInsets(top: 8,
                                     leading: 8,
@@ -40,58 +46,30 @@ struct ContentView: View {
                 .padding()
             }
             
-            HStack (spacing: 14) {
-                VStack {
-                    Image(systemName: "person.fill")
-                        .resizable()
-                        .frame(width: 30,
-                               height: 30)
-                        .padding(.all, 20)
-                        .background(Color.yellow)
-                        .foregroundColor(.white)
-                        .cornerRadius(100)  // 테두리를 둥글게 하는 건데 수치에 대해서는 잘 모르겠음
-                    // 크기의 반으로 해보았는데 둥글게 되지 않음
-                    Text("Polo")
-                        .font(.footnote)
-                }
-                VStack {
-                    Image(systemName: "person.fill")
-                        .resizable()
-                        .frame(width: 30,
-                               height: 30)
-                        .padding(.all, 20)
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(100)
-                    Text("Swimming")
-                        .font(.footnote)
-                }
-                VStack {
-                    Image(systemName: "person.fill")
-                        .resizable()
-                        .frame(width: 30,
-                               height: 30)
-                        .padding(.all, 20)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(100)
-                    Text("Dancing")
-                        .font(.footnote)
-                }
-                VStack {
-                    Image(systemName: "person.fill")
-                        .resizable()
-                        .frame(width: 30,
-                               height: 30)
-                        .padding(.all, 20)
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(100)
-                    Text("Singing")
-                        .font(.footnote)
+            // 카테고리
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack (spacing: 14) {
+                    ForEach(categories, id: \.self) { category in
+                        VStack {
+                            Image(systemName: "person.fill")
+                                .resizable()
+                                .frame(width: 30,
+                                       height: 30)
+                                .padding(.all, 20)
+                                .background(Color(red: .random(in: 0...1),
+                                                  green: .random(in: 0...1),
+                                                  blue: .random(in: 0...1)))
+                                .foregroundColor(.white)
+                                .cornerRadius(100)  // 테두리를 둥글게 하는 건데 수치에 대해서는 잘 모르겠음
+                            // 크기의 반으로 해보았는데 둥글게 되지 않음
+                            Text(category)
+                                .font(.footnote)
+                        }
+                    }
                 }
             }
             
+            // 분류
             HStack (spacing: 20) {
                 Text("Favorites")
                     .font(.system(size: 24))
@@ -105,65 +83,46 @@ struct ContentView: View {
             .padding(.vertical, 24)
             .padding()
             
+            // 리스트
             ScrollView(.vertical, showsIndicators: false) {
-                VStack {
-                    VStack (alignment: .leading, spacing: 20) {
-                        HStack {
-                            Image("photo")
-                                .resizable()
-                                .frame(width: 80,
-                                       height: 80)
-                                .cornerRadius(20)
-                            Spacer()
-                            Image(systemName: "arrowtriangle.right.fill")
-                                .resizable()
-                                .frame(width: 10, height: 10)
-                                .foregroundColor(.gray)
-                                .padding(.all, 24)
-                                .background(Color.white)
-                                .clipShape(Circle())
+                ForEach(0..<2) { _ in
+                    VStack {
+                        VStack (alignment: .leading, spacing: 20) {
+                            HStack {
+                                Image("photo")
+                                    .resizable()
+                                    .frame(width: 80,
+                                           height: 80)
+                                    .cornerRadius(20)
+                                Spacer()
+                                Button {
+                                    print("play")
+                                } label: {
+                                    Image(systemName: "arrowtriangle.right.fill")
+                                        .resizable()
+                                        .frame(width: 10, height: 10)
+                                        .foregroundColor(.gray)
+                                        .padding(.all, 24)
+                                        .background(Color.white)
+                                        .clipShape(Circle())
+                                }
+
+                            }
+                            
+                            Text("Singing Sensation")
+                                .font(.title)
+                                .fontWeight(.heavy)
+                            
+                            Text("After being enrolled in the Song festival, you will be a rockstar in no time")
+                                .foregroundColor(.secondary)
+                                .lineLimit(2)
                         }
-                        Text("Singing Sensation")
-                            .font(.title)
-                            .fontWeight(.heavy)
-                        Text("After being enrolled in the Song festival, you will be a rockstar in no time")
-                            .foregroundColor(.secondary)
-                            .lineLimit(2)
+                        .padding(.horizontal, 24)
                     }
-                    .padding(.horizontal, 24)
+                    .frame(height:230)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(36)
                 }
-                .frame(height:230)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(36)
-                VStack {
-                    VStack (alignment: .leading, spacing: 20) {
-                        HStack {
-                            Image("photo")
-                                .resizable()
-                                .frame(width: 80,
-                                       height: 80)
-                                .cornerRadius(20)
-                            Spacer()
-                            Image(systemName: "arrowtriangle.right.fill")
-                                .resizable()
-                                .frame(width: 10, height: 10)
-                                .foregroundColor(.gray)
-                                .padding(.all, 24)
-                                .background(Color.white)
-                                .clipShape(Circle())
-                        }
-                        Text("Singing Sensation")
-                            .font(.title)
-                            .fontWeight(.heavy)
-                        Text("After being enrolled in the Song festival, you will be a rockstar in no time")
-                            .foregroundColor(.secondary)
-                            .lineLimit(2)
-                    }
-                    .padding(.horizontal, 24)
-                }
-                .frame(height:230)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(36)
             }
         }
         .padding()
